ff641e673645be38d34dd8afe09b71fe14fa55e4
Szabolcs Nováczki <szabolcs.novaczki@nsn.com>
2015-05-31 07:03:52 +0200

    solution 1
    Result: C: 50%; P: 33%; S: 41%

---
+++ TapeEquilibrium.cpp
@@ -0,0 +1,34 @@
+// you can use includes, for example:
+// #include <algorithm>
+
+// you can write to stdout for debugging purposes, e.g.
+// cout << "this is a debug message" << endl;
+
+
+#include <cmath>
+
+int solution(vector<int> &A) {
+    int N = A.size();
+
+    int lower_sum = A[0];
+
+    int higher_sum = 0;
+    for( int i = 1; i < N; i++){
+        higher_sum += A[i];
+    }
+
+    int diff = abs( lower_sum - higher_sum );
+    int min_diff = diff;
+
+    for( int p = 1; p < (N-1); p++ ) {
+        diff = abs( diff - 2*A[p] );
+
+        if( diff < min_diff ) {
+             min_diff = diff;
+        }
+    }
+
+
+    return min_diff;
+
+}
